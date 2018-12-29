b269d09e260daf237b339d6b0b21c3c906c15350
Niklaus Zbinden <niklaus.zbinden@gmail.com>
2018-12-29 19:43:21 +0100

    Full setup

--- setup.sh
+++ setup.sh
@@ -3,17 +3,23 @@
 mkdir luxeriadoorbell
 cd luxeriadoorbell
 wget -q  https://github.com/nickik/tutuv9-client/blob/master/sound.mp3?raw=true 
-mv sound.mp3?raw=true sound.mp3
+mv -f sound.mp3?raw=true sound.mp3
+mv -f sound.mp3 /usr/sbin/
 
 wget -q https://github.com/nickik/tutuv9-client/blob/master/tutuv9-client?raw=true 
 mv tutuv9-client?raw=true client
 chmod +x client
-mv ./client /usr/sbin/luxeriadoorbellclient
+mv -f ./client /usr/sbin/luxeriadoorbellclient
 
 wget -q https://github.com/nickik/tutuv9-client/blob/master/tutu.service?raw=true
 mv tutu.service?raw=true tutu.service
 mv tutu.service /etc/systemd/system/
 chmod 664 /etc/systemd/system/tutu.service
+
+systemctl start tutu.service
+
+cd ..
+rm -rf luxeriadoorbell
 
 
 
